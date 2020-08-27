382f234c1bb52e5d603169aabd76df4569db5af1
24hdev <24hdev@24hdevs-Mac-mini.local>
2020-08-27 20:20:48 +0700

    Add custom scroll bars

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -13,11 +13,32 @@ const mapDispatchToProps = (dispatch) => ({
 });
 const Contacts = ({ title, showContacts, loading }) => {
   return (
-    <ModalContacts
-      title={title}
-      isOpen={showContacts}
-      isLoading={loading}
-    ></ModalContacts>
+    <ModalContacts title={title} isOpen={showContacts} isLoading={loading}>
+      {!isErrors && (
+        <CustomScrollbars
+          onReachedBottom={onReachedToBottom}
+          style={{ height: 350 }}
+        >
+          {contactsData.map((contact, id) => (
+            <div
+              key={id}
+              className="d-flex"
+              onClick={() => selectedContactActive(contact)}
+            >
+              <p className="mr-3">{contact.id}</p>
+              <p className="mr-3">
+                <strong>
+                  {contact.first_name} {contact.last_name}
+                </strong>
+              </p>
+              <p className="mr-3">{contact.email}</p>
+              <p>{contact.phone_number}</p>
+            </div>
+          ))}
+        </CustomScrollbars>
+      )}
+      {isErrors && "Error!"}
+    </ModalContacts>
   );
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
